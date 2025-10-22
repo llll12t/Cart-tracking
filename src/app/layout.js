425bec8f8bb7ade976e41370a07342fd50b423b9
llll12t <147978645+llll12t@users.noreply.github.com>
2025-10-23 01:20:04 +0700

    Add files via upload

---
+++ src/app/layout.js
@@ -0,0 +1,28 @@
+// src/app/layout.js
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { AuthProvider } from "@/context/AuthContext"; // 1. Import เข้ามา
+import PageTransition from '@/components/PageTransition';
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Fleet Management",
+  description: "ระบบจัดการรถในองค์กร",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      </head>
+      <body className={inter.className}>
+        <AuthProvider>
+          <PageTransition />
+          {children}
+        </AuthProvider>
+      </body>
+    </html>
+  );
+}
